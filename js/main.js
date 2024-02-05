6d223585a6b4bf078d3574209f99bf79a7fe2f51
das41k <armenpetrosan319@gmail.com>
2024-02-05 17:06:09 +0300

    update Header

---
+++ js/main.js
@@ -0,0 +1,14 @@
+const burgerIcon = document.querySelector('.nav__burger-icon')
+const burgerMenu = document.querySelector('.burger-menu')
+const kak = document.querySelector('.kak')
+burgerIcon.addEventListener('click', () => {
+    if (!burgerMenu.classList.contains('burger-menu--opened')) {
+        burgerMenu.classList.add('burger-menu--opened')
+        kak.classList.add('kak--closed')
+    }
+})
+const burgerMenuClose = document.querySelector('.burger-menu__cross')
+burgerMenuClose.addEventListener('click', () => {
+    burgerMenu.classList.remove('burger-menu--opened')
+    kak.classList.remove('kak--closed')
+})
